f9d11bfe8aa961eea4dd8aa5ced9c16758a6756f
Chumilion <2016esison@tjhsst.edu>
2015-11-09 15:07:54 -0500

    Initial commit

---
+++ app/src/main/java/chumilion/asynctasks/MainActivity.java
@@ -0,0 +1,86 @@
+package chumilion.asynctasks;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Chronometer;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public 
+}
+class ProgressBarTask extends AsyncTask<Integer, Float, String>
+{
+    ProgressBar progressBar;
+    protected String doInBackground(Integer... tim)
+    {
+        progressBar.setMax(tim[0]*1000);
+        long startTime = System.currentTimeMillis();
+        while(System.currentTimeMillis() - startTime < tim[0]*1000)
+        {
+            float frac = (System.currentTimeMillis() - startTime)/(1000*tim[0]);
+            publishProgress(frac);
+            return "Finished!"
+        }
+    }
+
+    protected void onProgressUpdate(Float... frac)
+    {
+        progressBar.setProgress();
+    }
+}
